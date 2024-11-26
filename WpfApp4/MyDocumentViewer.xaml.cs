bc02b170f9bb3bb5195c0af38ea3752911d41633
cclemon40 <ggwp6900899@gmail.com>
2024-11-26 17:34:04 +0800

    Ver1

--- WpfApp4/MyDocumentViewer.xaml.cs
+++ WpfApp4/MyDocumentViewer.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace WpfApp4
 {
@@ -19,9 +8,34 @@ namespace WpfApp4
     /// </summary>
     public partial class MyDocumentViewer : Window
     {
+        Color fontColor = Colors.Black;
         public MyDocumentViewer()
         {
             InitializeComponent();
+            fontColorPicker.SelectedColor = fontColor;
+            foreach (FontFamily fontFamily in Fonts.SystemFontFamilies)
+            {
+                fontFamilyComboBox.Items.Add(fontFamily.Source);
+            }
+            fontFamilyComboBox.SelectedItem = "Arial";
+            fontSizeComboBox.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+            fontSizeComboBox.SelectedIndex = 2;
+        }
+
+        private void NewCommand_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            MyDocumentViewer myDocumentViewer = new MyDocumentViewer();
+            myDocumentViewer.Show();
+        }
+
+        private void OpenCommand_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            
+        }
+
+        private void SaveCommand_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+
         }
     }
 }
